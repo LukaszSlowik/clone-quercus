0460d1ac2d5fde7e19553d00e393da795744703c
Lukasz <lukasz.slowik.osw@gmail.com>
2023-12-18 10:27:46 +0100

    error message last name

--- lib/validations/index.ts
+++ lib/validations/index.ts
@@ -3,7 +3,7 @@ import { z } from "zod";
 export const schemaCareers = z
   .object({
     firstName: z.string().min(1, { message: "First Name is required" }),
-    lastName: z.string().min(1, { message: "Last Name Name is required" }),
+    lastName: z.string().min(1, { message: "Last Name is required" }),
     email: z.string().email({ message: "Email is required" }),
     phone: z
       .string({
